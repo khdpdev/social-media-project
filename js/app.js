a51c718f365bf0b5dea9050ab117e2a16c75c05c
KDev <khdpdeveloper@gmail.com>
2026-06-30 06:36:45 -0400

    Declarado las variables globales y el inicializador dponde cree las propiedades y se las asigné a la variable global que va a capturar elementos HTML

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const appState = {
+    darkMode: false,
+    textTweet: []
+};
+
+const elementosDom = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Si está conectado');
+    elementosDom.body = document.body;
+    elementosDom.boton = document.querySelector('#dark-mode');
+
+    console.log('Capturado y asignado las propiedades a la constante elementosDOM: ', elementosDom);
+});
